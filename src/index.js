1aeb081c6fe9db24236cb26a9476646c1471b8d0
Aero48 <56200404+Aero48@users.noreply.github.com>
2023-11-06 16:03:59 -0500

    cleaned up some functions

--- src/index.js
+++ src/index.js
@@ -1,28 +1,10 @@
 import * as MODEL from "./model.js";
-import * as game from "./game.js";
-
-function changeRoute() {
-  let pageURL = window.location.hash.replace("#", "");
-  //Splits the page url up by slashes and puts the pieces into an array
-  let pageLayers = pageURL.split("/");
-
-  //Takes each piece of the array and assigns them to pageID and subpageID respectively
-  let pageID = pageLayers[0];
-  let subpageID = pageLayers[1];
-
-  //Loads different model functions depending on the pageID
-  if (pageID == "" || pageID == "home") {
-    MODEL.changePage(pageID);
-  } else {
-    MODEL.changePage(pageID);
-  }
-}
 
 function initListeners() {
-  $(window).on("hashchange", changeRoute);
-  changeRoute();
+  $(window).on("hashchange", MODEL.changeRoute);
 }
 
 $(document).ready(function () {
   initListeners();
+  MODEL.changeRoute();
 });
--- src/model.js
+++ src/model.js
@@ -11,11 +11,6 @@ import {
 
 import * as game from "./game.js";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAkOmRBIrS0ALZKZB5fPfAvgMVBabqC_SY",
   authDomain: "logcdoug-fb.firebaseapp.com",
@@ -34,37 +29,91 @@ let myp5;
 
 export var test = "test";
 
+// //FIREBASE TEST
+// export async function getAllData() {
+//   // gets all the data from the collection
+//   const querySnapshot = await getDocs(collection(db, "Albums"));
+//   let htmlStr = "";
+//   querySnapshot.forEach((doc) => {
+//     htmlStr += `<div class="album-item">
+//       <img src="${doc.data().photo}">
+//       <p class="name">Title: ${doc.data().title}</p>
+//       <p class="name">Artist: ${doc.data().artist}</p>
+//       <p class="name">Genre: ${doc.data().genre}</p>
+//       </div>`
+//   });
+//   console.log(htmlStr);
+
+//   return htmlStr;
+// }
+
 // Sets page content depending on pageID & subpageID. Also can run app.js callback functions for event listeners.
-export function changePage(pageID, subpageID, callback) {
-  if (pageID == "" || pageID == "home") {
-    $.get(`pages/home.html`, function (contents) {
-      $("#content").html(contents);
-      myp5.remove();
-    });
-  } else if (pageID == "play") {
-    $.get(`pages/play.html`, function (contents) {
-      $("#content").html(contents);
-      myp5 = new p5(game.s);
-    });
-  } else if (pageID == "pokedex") {
-    $.get(`pages/pokedex.html`, function (contents) {
-      $("#content").html(contents);
-      myp5.remove();
-    });
-  } else if (pageID == "storage") {
-    $.get(`pages/storage.html`, function (contents) {
-      $("#content").html(contents);
-      myp5.remove();
-    });
-  } else if (pageID == "trainers") {
-    $.get(`pages/trainers.html`, function (contents) {
-      $("#content").html(contents);
-      myp5.remove();
-    });
-  } else if (pageID == "login") {
-    $.get(`pages/login.html`, function (contents) {
-      $("#content").html(contents);
-      myp5.remove();
-    });
+function changePage(pageID, subpageID, callback) {
+  switch (pageID) {
+    case "home":
+      $.get(`pages/home.html`, function (contents) {
+        $("#content").html(contents);
+        if (myp5 != null) {
+          myp5.remove();
+        }
+      })
+      break;
+    case "play":
+      $.get(`pages/play.html`, function (contents) {
+        $("#content").html(contents);
+        myp5 = new p5(game.s);
+      });
+      break;
+    case "pokedex":
+      $.get(`pages/pokedex.html`, function (contents) {
+        $("#content").html(contents);
+        if (myp5 != null) {
+          myp5.remove();
+        }
+      });
+      break;
+    case "storage":
+      $.get(`pages/storage.html`, function (contents) {
+        $("#content").html(contents);
+        if (myp5 != null) {
+          myp5.remove();
+        }
+      });
+      break;
+    case "trainers":
+      $.get(`pages/trainers.html`, function (contents) {
+        $("#content").html(contents);
+        if (myp5 != null) {
+          myp5.remove();
+        }
+      });
+      break;
+    case "login":
+      $.get(`pages/login.html`, function (contents) {
+        $("#content").html(contents);
+        if (myp5 != null) {
+          myp5.remove();
+        }
+      });
+      break;
+    default:
+      $.get(`pages/home.html`, function (contents) {
+        $("#content").html(contents);
+        if (myp5 != null) {
+          myp5.remove();
+        }
+      });
   }
+}
+
+export function changeRoute() {
+  let pageURL = window.location.hash.replace("#", "");
+  //Splits the page url up by slashes and puts the pieces into an array
+  let pageLayers = pageURL.split("/");
+
+  //Takes each piece of the array and assigns them to pageID and subpageID respectively
+  let pageID = pageLayers[0];
+  let subpageID = pageLayers[1];
+
+  changePage(pageID);
 }
